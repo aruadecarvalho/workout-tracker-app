2ab89641682e76da85c9fde1517b1391ab19e4b6
aruadecarvalho <aruadecarvalho@gmail.com>
2022-12-04 14:55:02 -0300

    redux persistin in workout data

--- src/store/workout-data/workout-data.selector.js
+++ src/store/workout-data/workout-data.selector.js
@@ -1,6 +1,19 @@
-export const selectWorkoutData = (state) => state.workoutData.workoutData;
+import { createSelector } from "reselect";
 
-export const selectWorkoutNameAndType = (state) =>
+const selectWorkoutDataReducer = (state) => state.workoutData;
+
+// create a selector with redux persist to get the workout data from the store
+export const selectWorkoutData = createSelector(
+  [selectWorkoutDataReducer],
+  (workoutData) => workoutData.workoutData
+);
+
+const selectWorkoutNameAndTypeReducer = (state) =>
   state.workoutData.nameAndType;
+
+export const selectWorkoutNameAndType = createSelector(
+  [selectWorkoutNameAndTypeReducer],
+  (nameAndType) => nameAndType
+);
 
 export const selectCanSubmit = (state) => state.workoutData.canSubmitWorkout;
