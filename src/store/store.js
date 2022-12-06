ed1de321d94377810145ec20d91bed6e9c0b2d61
aruadecarvalho <aruadecarvalho@gmail.com>
2022-12-05 21:02:47 -0300

    setup redux-saga

--- src/store/store.js
+++ src/store/store.js
@@ -2,23 +2,39 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 import { rootReducer } from "./root-reducer";
+import { rootSaga } from "./root-saga";
 
-const middlewares = [logger];
 const persistConfig = {
   key: "root",
   storage,
   whitelist: ["workoutData"],
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const middlewares = [
+  process.env.NODE_ENV !== "production" && logger,
+  sagaMiddleware,
+].filter(Boolean);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
 export const store = createStore(
   persistedReducer,
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
